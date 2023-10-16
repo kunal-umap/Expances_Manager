f5fb7ea2c5f7a29463b3420bcfdabdf931de7f20
Kapil yadav <ky7501014@gmail.com>
2023-10-16 13:06:58 +0530

    Bottom bar half completed

--- lib/pages/Home/Home_Wedgets/BottomBar/bottom.dart
+++ lib/pages/Home/Home_Wedgets/BottomBar/bottom.dart
@@ -1,43 +1,21 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
-BottomAppBar bottomAppBar() {
-  return const BottomAppBar(
-    color: Colors.black,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Column(children: [
-          SizedBox(height: 2),
-          Icon(Icons.home, size: 30),
-          Text(
-            "Home",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 2)
-        ]),
-        Column(children: [
-          SizedBox(height: 2),
-          Icon(
-            Icons.money,
-            size: 30,
-            shadows: [],
-          ),
-          Text(
-            "Transaction",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 2),
-        ]),
-        Column(children: [
-          SizedBox(height: 2),
-          Icon(Icons.cloud, size: 30),
-          Text(
-            "Dummy",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 2),
-        ])
-      ],
-    ),
-  );
+class Bottom_bar extends StatefulWidget {
+  const Bottom_bar({super.key});
+
+  @override
+  State<Bottom_bar> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Bottom_bar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+      ]),
+    );
+  }
 }
