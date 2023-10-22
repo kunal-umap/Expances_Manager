bcafe0a938ebe8a4ba7e6ac7a47877aa198586f5
Kunal Umap <kunalgumap@gmail.com>
2023-10-22 21:42:00 +0530

    Minor Changes and Fix In transaction

--- lib/pages/AddTransactionPage/Add_TransactionPage.dart
+++ lib/pages/AddTransactionPage/Add_TransactionPage.dart
@@ -57,52 +57,53 @@ class _add_TransactionState extends State<add_Transaction> {
           ),
         ),
       ),
-      body: ListView(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           const SizedBox(height: 20),
           Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Type",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                  const SizedBox(width: 75),
-                  SizedBox(
-                    width: 250,
-                    child: DropdownButton<String>(
-                        hint: const Text("Select type"),
-                        value: listItemSelected,
-                        isExpanded: true,
-                        items: _listItem.map((String dropDownStringItem) {
-                          return DropdownMenuItem<String>(
-                            value: dropDownStringItem,
-                            child: Text(dropDownStringItem),
-                          );
-                        }).toList(),
-                        onChanged: (newValueSelected) {
-                          setState(() {
-                            listItemSelected = newValueSelected!;
-                          });
-                        }),
-                  )
-                ],
-              )),
-          const SizedBox(height: 10),
+            padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  "Type",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                const SizedBox(width: 75),
+                Expanded(
+                  // width: 150,
+                  child: DropdownButton<String>(
+                      hint: const Text("Select type"),
+                      value: listItemSelected,
+                      isExpanded: true,
+                      items: _listItem.map((String dropDownStringItem) {
+                        return DropdownMenuItem<String>(
+                          value: dropDownStringItem,
+                          child: Text(dropDownStringItem),
+                        );
+                      }).toList(),
+                      onChanged: (newValueSelected) {
+                        setState(() {
+                          listItemSelected = newValueSelected!;
+                        });
+                      }),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 2),
           Padding(
-              padding: const EdgeInsets.fromLTRB(20, 5, 16, 5),
+              padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   const Text(
                     "Mode",
                     style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                   const SizedBox(width: 70),
-                  SizedBox(
-                    width: 250,
+                  Expanded(
                     child: DropdownButton<String>(
                         hint: const Text("Select Mode"),
                         value: modechoose,
@@ -121,33 +122,41 @@ class _add_TransactionState extends State<add_Transaction> {
                   )
                 ],
               )),
-          const SizedBox(height: 30),
+          const SizedBox(height: 10),
           const Padding(
-              padding: EdgeInsets.fromLTRB(20, 5, 16, 5),
+              padding: EdgeInsets.fromLTRB(20, 12, 20, 12),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    "Discription",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      "Discription",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
                   ),
-                  SizedBox(width: 25),
-                  SizedBox(width: 250, height: 20, child: TextField()),
+                  // SizedBox(width: 25),
+                  Expanded(
+                    flex: 4,
+                    child: SizedBox(
+                      height: 18,
+                        child: TextField()
+                    ),
+                  ),
                 ],
               )),
-          const SizedBox(height: 35),
+          const SizedBox(height: 10),
           Padding(
-              padding: const EdgeInsets.fromLTRB(20, 5, 16, 5),
+              padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const Text(
                     "Category",
                     style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                   const SizedBox(width: 40),
-                  SizedBox(
-                    width: 250,
+                  Expanded(
                     child: DropdownButton<String>(
                         dropdownColor: Colors.black,
                         isExpanded: true,
@@ -166,100 +175,106 @@ class _add_TransactionState extends State<add_Transaction> {
                         }),
                   )
                 ],
-              )),
+              )
+          ),
           const SizedBox(height: 10),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16, 5, 16, 5),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(right: 95, bottom: 5),
-                      child: Text(
-                        "Date",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ),
-                    SizedBox(
-                        width: 150,
-                        height: 50,
-                        child: TextField(
-                          keyboardType: TextInputType.none,
-                          style: const TextStyle(fontSize: 16),
-                          controller: _date,
-                          decoration: const InputDecoration(
-                            prefixIcon: Padding(
-                              padding: EdgeInsets.only(right: 16),
-                              child: Icon(
-                                Icons.calendar_month_outlined,
-                              ),
-                            ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 12, 20, 12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Date",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
                           ),
-                          onTap: () async {
-                            DateTime? datapicked = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(2002),
-                                lastDate: DateTime(2030));
-                            if (datapicked != null) {
-                              setState(() {
-                                _date.text =
-                                    DateFormat("yyyy-MM-dd").format(datapicked);
-                              });
-                            }
-                          },
-                        ))
-                  ],
-                ),
-              ),
-              const VerticalDivider(
-                color: Colors.green,
-                thickness: 1,
-                indent: 40,
-                endIndent: 100,
-              ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(50, 5, 16, 5),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 23),
-                      child: SizedBox(
-                          width: 140,
-                          height: 35,
-                          child: TextField(
-                            keyboardType: TextInputType.number,
-                            style: TextStyle(fontSize: 20),
-                            decoration: InputDecoration(
-                              prefixIcon: Padding(
-                                padding: EdgeInsets.only(right: 16),
-                                child: Icon(
-                                  Icons.currency_rupee_rounded,
+                        ),
+                        SizedBox(
+                            width: 150,
+                            height: 40,
+                            child: TextField(
+                              keyboardType: TextInputType.none,
+                              style: const TextStyle(fontSize: 16),
+                              controller: _date,
+                              decoration: const InputDecoration(
+                                prefixIcon: Padding(
+                                  padding: EdgeInsets.only(right: 16),
+                                  child: Icon(
+                                    Icons.calendar_month_outlined,
+                                  ),
                                 ),
                               ),
-                            ),
-                          )),
-                    )
-                  ],
+                              onTap: () async {
+                                DateTime? datapicked = await showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(2002),
+                                    lastDate: DateTime(2030));
+                                if (datapicked != null) {
+                                  setState(() {
+                                    _date.text =
+                                        DateFormat("yyyy-MM-dd").format(datapicked);
+                                  });
+                                }
+                              },
+                            )
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                const Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 28),
+                          child: SizedBox(
+                              height: 35,
+                              child: TextField(
+                                keyboardType: TextInputType.number,
+                                style: TextStyle(fontSize: 20),
+                                decoration: InputDecoration(
+                                  prefixIcon: Padding(
+                                    padding: EdgeInsets.only(right: 12),
+                                    child: Icon(
+                                      Icons.currency_rupee_rounded,
+                                    ),
+                                  ),
+                                ),
+                              )
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          const SizedBox(
-            height: 20,
-          ),
+          Expanded(child: SizedBox()),
           SizedBox(
-            width: 327,
             height: 50,
+            width: double.maxFinite,
             child: ElevatedButton(
               onPressed: () {},
               child: const Text(
                 "Add",
                 style: TextStyle(
-                    color: Color.fromARGB(255, 160, 25, 184), fontSize: 18),
+                    color: Color.fromARGB(255, 160, 25, 184),
+                    fontSize: 18
+                ),
               ),
             ),
           )
